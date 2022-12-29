42d8b6bf722b0c497930edd65a580cfcfe015a19
Leydi Velazco <leydi.velazco@outlook.es>
2022-12-28 19:58:21 -0500

    Add getCard function

---
+++ assets/js/app.js
@@ -0,0 +1,56 @@
+/**
+ * 2T = Two of Clubs (Tréboles)
+ * 2D= Two of Diamonds (Diamantes)
+ * 2H = Two of Hearts (Corazones)
+ * 2S = Two of Spades (Espadas)
+ */
+
+let deck = [];
+let types = ['T', 'D', 'H', 'S'];
+let specials = ['A', 'J', 'K', 'Q'];
+
+//Función que crea una nueva baraja
+function createDeck() {
+    for (let i = 2; i < 11; i++) {
+        for (const type of types) {
+            deck.push(i + type);
+        }        
+    }
+
+    for (const special of specials) {
+        for (const type of types) {
+            deck.push(special + type);
+        }
+    }
+
+    // console.log(deck);
+
+    deck = _.shuffle( deck );
+
+    console.log(deck);
+
+    return deck;
+}
+
+createDeck();
+
+//Función que permite tomar una carta de la baraja
+const getCard = () => {
+
+    if (deck.length === 0) {
+        throw 'Error: No hay cartas en la baraja.';
+    } 
+
+    const card = deck.pop();
+        
+    console.log(deck);
+    console.log(card);
+
+    return card;
+}
+
+getCard();
+
+
+
+
