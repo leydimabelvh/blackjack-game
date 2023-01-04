aff315665358e671e28a17909acfc73d313ec9f7
Leydi Velazco <leydi.velazco@outlook.es>
2023-01-04 13:12:56 -0500

    Add function to determine the final result

--- assets/js/app.js
+++ assets/js/app.js
@@ -86,10 +86,32 @@
     playersCards[shift].append(imageCard);
   };
 
+  const determinateResult = () => {
+
+    const [minimumPoints, computerPoints] = playersPoints ;
+
+    setTimeout(() => {
+        //Mostrar resultados
+        if (computerPoints > 21) {
+          alert("¡Ganó el jugador!");
+          console.warn("¡Ganó el jugador!");
+        } else if (minimumPoints > 21) {
+          alert("¡Ganó la computadora!");
+          console.warn("¡Ganó la computadora!");
+        } else if (computerPoints === minimumPoints) {
+          alert("Empate, niguno ganó.");
+          console.warn("Empate, niguno ganó");
+        } else {
+          alert("¡Ganó la computadora!");
+          console.warn("¡Ganó la computadora!");
+        }
+      }, 100);
+  }
+
   const generateComputerShift = (minimumPoints) => {
     do {
       const card = getCard();
-      
+
       //Acumular puntos de juagdor
       accumulatePoints(card, playersPoints.length - 1);
 
@@ -101,22 +123,7 @@
       }
     } while (computerPoints < minimumPoints && minimumPoints <= 21);
 
-    setTimeout(() => {
-      //Mostrar resultados
-      if (computerPoints > 21) {
-        alert("¡Ganó el jugador!");
-        console.warn("¡Ganó el jugador!");
-      } else if (minimumPoints > 21) {
-        alert("¡Ganó la computadora!");
-        console.warn("¡Ganó la computadora!");
-      } else if (computerPoints === minimumPoints) {
-        alert("Empate, niguno ganó.");
-        console.warn("Empate, niguno ganó");
-      } else {
-        alert("¡Ganó la computadora!");
-        console.warn("¡Ganó la computadora!");
-      }
-    }, 100);
+    determinateResult();
   };
 
   //Eventos
